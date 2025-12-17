8922faa15698d1e2d35fe4cb637f9e5cb9f645eb
Nauman <naumansqb@gmail.com>
2025-12-17 13:25:02 -0500

    Shopping Cart Controller and DAO preliminary work setup - Added CRUD endpoints to ShoppingCartController
    - DAO methods and Controller methods not implemented yet

---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,44 @@
+package org.yearup.data.mysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    private ProductDao productDao;
+
+    @Autowired
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
+        super(dataSource);
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public void addProduct(int userId, int productId) {
+    }
+
+    @Override
+    public void updateQuantity(int userId, int productId, int quantity) {
+    }
+
+    @Override
+    public void clearCart(int userId) {
+    }
+}

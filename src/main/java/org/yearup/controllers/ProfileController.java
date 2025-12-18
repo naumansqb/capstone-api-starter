e9470fd016fcb29836dc9b6d2c6223e35435112c
Nauman <naumansqb@gmail.com>
2025-12-17 22:41:02 -0500

    Setup of Profile CRUD operations - DAO and Controller set up    - Logic needs to be implemented

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,65 @@
+package org.yearup.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+@CrossOrigin
+public class ProfileController {
+
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    public ProfileController(ProfileDao profileDao, UserDao userDao)
+    {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping
+    public Profile getProfile(Principal principal)
+    {
+        try
+        {
+            int userId = getUserId(principal);
+            return null;
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    @PutMapping
+    public Profile updateProfile(@RequestBody Profile profile, Principal principal)
+    {
+        try
+        {
+            int userId = getUserId(principal);
+            return null;
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    private int getUserId(Principal principal)
+    {
+        String userName = principal.getName();
+        User user = userDao.getByUserName(userName);
+        if(user == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
+        return user.getId();
+    }
+}
--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -16,6 +16,11 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
     }
 
     @Override
+    public Profile getProfile(int userId) {
+        return null;
+    }
+
+    @Override
     public Profile create(Profile profile)
     {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
@@ -42,6 +47,11 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void update(int userId, Profile profile) {
+
     }
 
 }
